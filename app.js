db983ce45e5f139f4b823d3544816dbc964e6531
justbraf <dwaynecorey@gmail.com>
2024-02-29 14:12:01 -0400

    implement DB

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 import express from 'express'
+import { PORT, MongoDBURL } from './config.js'
+import { MongoClient, ServerApiVersion } from "mongodb"
 const app = express()
-
-const PORT = 3000
 
 app.use(express.json())
 
-app.listen(PORT, ()=> {
+const client = new MongoClient(MongoDBURL,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const booksDB = client.db("myBookShop")
+const myBooks = booksDB.collection("booksCollection")
+
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
 
-app.get('/', (req, res)=> {
-    res.status(200).send("<h1>Hello there, how are you?!</h1>")
+app.get('/', (req, res) => {
+    return res.status(200).send("<h1>Hello there, how are you?!</h1>")
 })
 
-app.get('/shop', (req, res)=> {
-    res.status(232).send("<a href='/'> Home</a>")
+app.get('/shop', (req, res) => {
+    return res.status(200).send("<a href='/'> Home</a>")
 })
 
-app.get('/shop/:id', (req, res)=> {
+app.get('/shop/:id', (req, res) => {
     const data = req.params
-    res.status(232).send(`<a href='/'> Book: ${data.id}</a>`)
+    return res.status(200).send(`<a href='/'> Book: ${data.id}</a>`)
+})
+
+app.post('/savebook', (req, res) => {
+    const data = req.body
+    if (!data.title)
+        return res.status(400).send("No title found.")
+    if (!data.author)
+        return res.status(400).send("No author found.")
+    if (!data.price)
+        return res.status(400).send("No price found.")
+
+    myBooks.insertOne(data, (error, response)=>{
+        if(error){
+            console.log("An error occurred!")
+            return res.sendStatus(500)
+        }
+    })
+    return res.status(201).send(JSON.stringify(data))
 })
