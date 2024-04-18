820e938abbe79f503a10ff4e6974cbd28d98b86a
justbraf <dwaynecorey@gmail.com>
2024-04-18 17:32:30 -0400

    added React w/Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-amber-500 text-6xl text-center'>Welcome to the Book Shop</h1>
+    </>
+  )
+}
+
+export default App
